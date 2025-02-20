4fdc33a02635e28f64c5c86a046dfb4872d1017b
Olena-Koval <19111982e@gmail.com>
2025-02-20 12:45:20 +0100

    Agrega la ruta /home y HomeController

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+// Se agregó la importación de HomeController al inicio 
+use App\Http\Controllers\HomeController; // Importar HomeController
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Definir la ruta principal home
+// Se definió la ruta 
+Route::get('/home', [HomeController::class, 'index'])->name('home');
 
 // Incluir las rutas de autenticación de Breeze
 require __DIR__.'/auth.php';
